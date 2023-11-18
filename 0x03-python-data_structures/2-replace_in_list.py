882ef17b92c8c92c8134565c3446f32a622b542d
Modupi <modupimaphoto@gmail.com>
2023-11-18 02:12:56 -0800

    2. Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+def replace_in_list(my_list, idx, element):
+    length = len(my_list) - 1
+    if idx < 0 or idx > length:
+        return mylist
+    my_list[idx] = element
+    return my_list
