e26a7b4f2d8feb77118b82d19af2b12172e27141
Modupi <modupimaphoto@gmail.com>
2023-11-18 02:17:16 -0800

    Replace statement placed in else statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,6 @@ def replace_in_list(my_list, idx, element):
     length = len(my_list) - 1
     if idx < 0 or idx > length:
         return mylist
-    my_list[idx] = element
-    return my_list
+    else:
+        my_list[idx] = element
+        return my_list
