f4b78586ed43a197b03f88ef50ebc6a40c6caed2
Modupi <modupimaphoto@gmail.com>
2023-11-18 12:13:03 -0800

    Return the differences

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    list_1 = list(set_1)
-    list_2 = list(set_2)
-    list_1 += list_2
-    
-    return set(list_1)
+    return set_1.symmetric_difference(set_2)
