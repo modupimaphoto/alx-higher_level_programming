c71ec9ece3d18e4b88c2e717e3e2a9e24626fa3f
Modupi <modupimaphoto@gmail.com>
2023-11-18 11:30:49 -0800

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return
+
+    new_list = my_list[:]
+
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+
+    return new_list
