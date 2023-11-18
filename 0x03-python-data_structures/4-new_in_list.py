0375f727eef4377b00474a1fd5c332c6fd1f5fce
Modupi <modupimaphoto@gmail.com>
2023-11-18 02:45:39 -0800

    4. Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+def new_in_list(my_list, idx, element):
+    list_cpy = my_list[:]
+    length = len(list_cpy) - 1
+
+    if idx < 0 or idx > length:
+        return list_cpy
+    else:
+        list_cpy[idx] = element
+        return list_cpy
