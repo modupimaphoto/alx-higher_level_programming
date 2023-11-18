2e781aba62884039f2e178c98db5174bcdecd8c7
Modupi <modupimaphoto@gmail.com>
2023-11-18 03:54:38 -0800

    Fix 1

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,10 +2,10 @@
 
 def multiple_returns(sentence):
 
-    if len(sentence) == 0:
-        sentence[0] = None
+    if sentence == "":
+        return (0, None)
 
     tuple_s = (sentence)
-    first_c = tuple_s[0]
+    first_c = sentence[0]
 
     return (len(tuple_s), first_c)
