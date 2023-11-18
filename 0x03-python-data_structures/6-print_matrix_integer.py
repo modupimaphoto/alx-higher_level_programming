f741313c353db029727907e1141ef9984c752350
Modupi <modupimaphoto@gmail.com>
2023-11-18 03:19:15 -0800

    Fix 1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,11 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" ")
-        
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][col]), end="")
+            if col != (len(matrix[row]) - 1):
+                print(" ", end=" ")
+
         print("")
+
