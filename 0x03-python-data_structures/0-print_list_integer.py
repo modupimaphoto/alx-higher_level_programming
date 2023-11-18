c709f3b1221484dd7ade421e08f64a96a43e7378
Modupi <modupimaphoto@gmail.com>
2023-11-18 02:02:28 -0800

    Expecting integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{}".format(i))
+    length = len(my_list)
+    for i in range(length):
+        print("{:d}".format(my_list[i]))
