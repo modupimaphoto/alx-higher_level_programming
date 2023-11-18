1040068c4a02a25b55669003d3a61b7b133264a6
Modupi <modupimaphoto@gmail.com>
2023-11-18 03:52:04 -0800

    8. More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 
+def multiple_returns(sentence):
+
+    if len(sentence) == 0:
+        sentence[0] = None
+
+    tuple_s = (sentence)
+    first_c = tuple_s[0]
+
+    return (len(tuple_s), first_c)
