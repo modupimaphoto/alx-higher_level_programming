68eaeb819ea6bff6935813eace9998a967dc3efb
Modupi <modupimaphoto@gmail.com>
2023-11-18 03:02:32 -0800

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+def no_c(my_string):
+    modified_string = ""
+
+    for letter in my_string:
+        if letter.lower() != 'c':
+            modified_string += letter
+
+    return modified_string
