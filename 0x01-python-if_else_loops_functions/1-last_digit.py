89191324ede40e3cbeec36c9e5306bc87a597897
modupimaphoto <modupimaphoto@gmail.com>
2023-11-29 09:26:31 -0800

    Fix NameError:

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number < 0:
 elif number >= 0:
     last_di = number % 10
 
-if last_digit > 5:
+if last_di > 5:
     print(f"Last digit of {number} is {last_di} and is greater than 5")
 elif last_di == 0:
     print(f"Last digit of {number} is {last_di} and is 0")
