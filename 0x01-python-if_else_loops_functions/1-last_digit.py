d4ffc2d7ba8e194ba75c2e3171be2c317be4759e
modupimaphoto <modupimaphoto@gmail.com>
2023-11-29 09:05:01 -0800

    Correct syntax

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    last_digit number % -10
-elif number => 0:
+    last_digit = number % -10
+elif number >= 0:
     last_digit = number % 10
 
 if last_digit > 5:
