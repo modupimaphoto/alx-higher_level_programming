0930fc3e1b70cc9583733a12004b60a33c859327
modupimaphoto <modupimaphoto@gmail.com>
2023-12-01 07:08:45 -0800

    Changed len variable to len

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    len = len(sys.argv) - 1
-    if len == 0:
-        print("{} arguments.".format(len))
-    elif len == 1:
-        print("{} argument:".format(len))
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{} argument:".format(len))
-    if len >= 1:
+        print("{} argument:".format(i))
+    if i >= 1:
         i = 0
         for arg in sys.argv:
             if i != 0:
