6f6b0480daa3fa4f660ca79d0885d0c78a71a133
Modupi <modupimaphoto@gmail.com>
2023-12-18 14:03:50 -0800

    7. Safe integer print with error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return False
