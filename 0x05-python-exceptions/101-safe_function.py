7180dbd0c98291a152b67be42c62f3551ab98903
Modupi <modupimaphoto@gmail.com>
2023-12-18 14:07:03 -0800

    8. Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
 def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+        return result
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
