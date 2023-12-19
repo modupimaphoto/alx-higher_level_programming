3ead0e74714a5f1650712f3ed0a6c075ad9f0d89
Modupi <modupimaphoto@gmail.com>
2023-12-19 12:10:54 -0800

    Correction

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,9 @@ class Square:
         Intialize a new Square.
         Args: size (int): The size of the new square
         """
-        self.__size = size
 
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if self.__size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
