c2e93ec0951b97aacd7ddaa49d8c1d4231eb4db0
Modupi <modupimaphoto@gmail.com>
2023-12-19 11:22:18 -0800

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+class Square:
+    """Square class body"""
+
+    def __init__(self, size):
+        """
+        Intialize a new Square.
+        Args: size (int): The size of the new square.
+        """
+        self.__size = size
