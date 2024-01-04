c0b7f2ab604b2cdabab3711605091e27bf648306
Modupi <modupimaphoto@gmail.com>
2024-01-04 10:40:35 -0800

    1. Real definition of a rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""1-rectangle module"""
+
+
+class Rectangle:
+    """module body"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width: must be integer
+            Args:
+                value: the width
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height: must be integer
+            Args:
+                value: the height
+        """
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
