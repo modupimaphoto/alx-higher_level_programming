5135942a1c172257eb2b979373f342197b10af36
Modupi <modupimaphoto@gmail.com>
2024-01-08 10:37:23 -0800

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""prints the list, but sorted (ascending sort)"""
+
+
+class MyList(list):
+    """Class methods and attributes"""
+
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(self)
