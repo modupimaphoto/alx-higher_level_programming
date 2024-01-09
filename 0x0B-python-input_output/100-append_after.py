245bc87c52c3adca07795fa3fd0be0c8896a1a66
Modupi <modupimaphoto@gmail.com>
2024-01-09 18:15:09 +0200

    13. Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines function that inserts a line"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append_after function
+    inserts a line of text to a file,
+    after each line containing a specific string
+    """
+    text = ""
+    with open(filename, "r+") as file:
+        for line in file:
+            text += line
+            if search_string in line:
+                text += new_string
+        file.write(text)
