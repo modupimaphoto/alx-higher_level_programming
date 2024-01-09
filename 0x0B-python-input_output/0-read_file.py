6d8608fb70084663326a45550e83a010a2f0e5b4
Modupi <modupimaphoto@gmail.com>
2024-01-09 14:38:28 -0800

    Removes last line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Defines read file module"""
+
 
 def read_file(filename=""):
-
+    """read_file function
+    reads a text file (UTF8) and prints it result to stdout
+    """
     with open(filename, "r") as file:
         content = file.read()
-        print(content)
+        print(content, end="")
