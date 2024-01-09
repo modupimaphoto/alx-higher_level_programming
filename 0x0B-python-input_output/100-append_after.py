030d6b7f20bb8d22cdcba962f4391a4bf5ae42e3
Modupi <modupimaphoto@gmail.com>
2024-01-09 18:26:12 +0200

    Correction

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,9 +8,10 @@ def append_after(filename="", search_string="", new_string=""):
     after each line containing a specific string
     """
     text = ""
-    with open(filename, "r+") as file:
+    with open(filename, "r") as file:
         for line in file:
             text += line
             if search_string in line:
                 text += new_string
+    with open(filename, "w") as file:
         file.write(text)
