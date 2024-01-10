ab366f78cb1a9c30c0d074fcb0826699b19d99d8
Modupi <modupimaphoto@gmail.com>
2024-01-09 16:38:15 -0800

    Correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""adds all arguments to a Python list,
+and then save them to a file:
+"""
 from sys import argv
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -10,5 +14,6 @@ if __name__ == "__main__":
         items = load_from_json_file('add_item.json')
     except FileNotFoundError:
         items = []
-    items.extend(argv[1:])
+    argv.pop(0)
+    items.extend(argv)
     save_to_json_file(items, 'add_item.json')
