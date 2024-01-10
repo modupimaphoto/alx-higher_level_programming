97f0c3cbc3324b713689e51f142e99e86132516b
Modupi <modupimaphoto@gmail.com>
2024-01-10 22:26:12 +0200

    13. Rectangle instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,3 +127,14 @@ class Rectangle(Base):
             self.__y = args[4]
         except IndexError:
             pass
+
+    def to_dictionary(self):
+        """returns the dictionary representation of a Rectangle:"""
+        rep_dict = {
+            'x': self.__x,
+            'y': self.__y,
+            'id': self.id,
+            'height': self.__height,
+            'width': self.__width
+        }
+        return rep_dict
