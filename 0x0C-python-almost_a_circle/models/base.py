377b6e765373ddb83f36bc238c7aea43e086c1d1
Modupi <modupimaphoto@gmail.com>
2024-01-12 12:26:43 +0200

    15. Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """Defines base module"""
 
 
@@ -15,3 +16,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation
+        of list_dictionaries:
+        Args:
+            list_dictionaries is a list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '"[]"'
+        return json.dumps(list_dictionaries)
