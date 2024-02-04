c543b89219cfd54cefb6893aea5718864c0835bd
Modupi <modupimaphoto@gmail.com>
2024-02-04 20:06:57 +0200

    Correction

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- This script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABSASE IF NOT EXISTS hbtn_0d_usa
+USE hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS cities(
 id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 state_id INT FOREIGN KEY(id) REFERENCES states(id),
