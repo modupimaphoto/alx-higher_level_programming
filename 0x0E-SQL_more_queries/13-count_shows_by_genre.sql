257c6a26091c5c506eb687571b4be0e9541f8ad2
Modupi <modupimaphoto@gmail.com>
2024-02-04 21:30:02 +0200

    Correction

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,5 @@
 -- displays the number of shows linked to each
 SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY number_of_shows DESCSELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC
