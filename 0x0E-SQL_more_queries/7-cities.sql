40d03a9d92ac54abe16f0629228fbd1fb9b7bccb
Modupi <modupimaphoto@gmail.com>
2024-02-04 21:05:13 +0200

    Correction

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- This script that creates the database hbtn_0d_usa and the table cities
 CREATE DATABSASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-id INT AUTO_INCREMENT PRIMARY KEY,
-state_id INT FOREIGN KEY(id) REFERENCES states(id),
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+state_id INT FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
 name VARCHAR(256) NOT NULL
 );
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- This script that lists all the cities of California
-SELECT cities.id, cities.name FROM cities
-WHERE (SELECT name FROM states WHERE name = "California ")
-ORDER BY cities.id
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = "California")
+ORDER BY id
