acdf4efffedf9fffbe916038c5522b16447d5435
Modupi <modupimaphoto@gmail.com>
2024-02-04 08:48:32 +0200

    Correction

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- This script that inserts a new row in the table first_table
-INSERT INTO first_table (id, name) VALUES(id=89, name='Best School')
+INSERT INTO first_table (id, name) VALUES(89,'Best School')
