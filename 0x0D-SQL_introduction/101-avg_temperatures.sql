dc39164472a0af173ec3147057934190e9942852
Modupi <modupimaphoto@gmail.com>
2024-02-04 09:47:50 +0200

    18. Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- This script that displays the average temperature (Fahrenheit)
+-- by city ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp FROM  AVG(value) AS avg_temp FROM temperatures
+GROUP BY city
+ORDER BY value DESC temperatures
+GROUP BY city
+ORDER BY value DESC
