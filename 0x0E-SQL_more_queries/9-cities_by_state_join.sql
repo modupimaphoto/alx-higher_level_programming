e860737490a81f56c949387a396758241717dd50
Modupi <modupimaphoto@gmail.com>
2024-02-04 20:26:29 +0200

    9. Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- This script that lists all cities contained in the database hbtn_0d_usa.
+SELECT cities.id, cities.name, states.name FROM cities
+RIGHT JOIN states ON cities.id = states.state_id
+ORDER BY cities.id
