6b87c6c42c2511cd165d585e6e60670499c33977
Modupi <modupimaphoto@gmail.com>
2024-03-11 06:48:29 +0200

    2. Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+A script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+
+    host = 'localhost'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    port = 3306
+
+    c = db.cursor()
+    sql = "SELECT * FROM states WHERE name LIKE BINARY '{}'"
+    c.execute(sql.format(sys.argv[4]))
+
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
