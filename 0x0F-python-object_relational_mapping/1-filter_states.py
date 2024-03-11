9ebdde9bdb668f96891122a1a5dcb46a86f59093
Modupi <modupimaphoto@gmail.com>
2024-03-11 06:36:58 +0200

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa:
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    host = 'localhost'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    port = 3306
+
+    db = MySQLdb.connect(host, user, password, db, port)
+    c = db.cursor()
+    sql = """SELECT * FROM states WHERE name
+    LIKE BINARY 'N%' ORDER BY states.id"""
+    c.execute()
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+
+    c.close()
+    db.close()
