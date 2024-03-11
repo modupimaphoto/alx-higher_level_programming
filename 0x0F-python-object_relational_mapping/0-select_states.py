bda18db780fa2b4c46b0fde0561c8475532220f0
Modupi <modupimaphoto@gmail.com>
2024-03-11 06:18:33 +0200

    Correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,13 @@ import MySQLdb
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    host = "localhost"
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    port = 3306
+
+    db = MySQLdb.connect(host, user, password, db, port)
 
     c = db.cursor()
     c.execute("SELECT * FROM states")
